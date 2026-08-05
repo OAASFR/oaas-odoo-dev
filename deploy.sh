a5d104127cc167eff985e1ab4cd4856ed1961024
Benjamin Prunier <bprunier@oaas.fr>
2026-08-05 15:45:26 +0200

    fix(deploy): corrige l'IP autorisée sur /web/database
    Coquille dans le commit précédent : 1.3.28.138 -> 2.3.28.138.

--- deploy.sh
+++ deploy.sh
@@ -324,7 +324,7 @@ sudo tee "${APACHE_SITE_FILE}" > /dev/null <<'EOF'
     </Directory>
 
     <Location /web/database>
-       Require ip 1.3.28.138
+       Require ip 2.3.28.138
     </Location>
 
     ErrorLog ${APACHE_LOG_DIR}/__DOMAIN__-error.log
